3a0ae9511b0b4501110d16824627b8cf0cc5f765
isen <tim.montety@hotmail.fr>
2017-05-16 16:07:39 +0200

    Coucou Erreur local ?

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from .forms import PostForm,CommentForm
 from django.contrib.auth.decorators import login_required
 
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -87,5 +88,4 @@ def comment_remove(request, pk):
     return redirect('post_detail', pk=comment.post.pk)
 
 def test(request):
-    form = CommentForm()
-    return render('blog/test.html', {'form': form})
+    return render(request,'blog/test.html', {})
