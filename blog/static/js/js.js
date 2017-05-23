5f74e17064d0e9db108efbe5b24a70d29356bc16
isen <tim.montety@hotmail.fr>
2017-05-23 15:42:24 +0200

    Test

--- blog/static/js/js.js
+++ blog/static/js/js.js
@@ -1,75 +1,69 @@
-var maxRadius = 32, // maximum radius of circle
-    padding = 1, // padding between circles; also minimum radius
-    margin = {top: -maxRadius, right: -maxRadius, bottom: -maxRadius, left: -maxRadius},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
 
-var k = 1, // initial number of candidates to consider per circle
-    m = 10, // initial number of circles to add per frame
-    n = 2500, // remaining number of circles to add
-    newCircle = bestCircleGenerator(maxRadius, padding);
+var width = 960,
+    height = 960;
 
-var svg = d3.select("body").append("svg")
+var projection = d3.geo.orthographic()
+    .translate([width / 2, height / 2])
+    .scale(width / 2 - 20)
+    .clipAngle(90)
+    .precision(0.6);
+
+var canvas = d3.select("body").append("canvas")
     .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("height", height);
 
-d3.timer(function() {
-  for (var i = 0; i < m && --n >= 0; ++i) {
-    var circle = newCircle(k);
+var c = canvas.node().getContext("2d");
 
-    svg.append("circle")
-        .attr("cx", circle[0])
-        .attr("cy", circle[1])
-        .attr("r", 0)
-        .style("fill-opacity", (Math.random() + .5) / 2)
+var path = d3.geo.path()
+    .projection(projection)
+    .context(c);
+
+var title = d3.select("h1");
+
+queue()
+    .defer(d3.json, "/mbostock/raw/4090846/world-110m.json")
+    .defer(d3.tsv, "/mbostock/raw/4090846/world-country-names.tsv")
+    .await(ready);
+
+function ready(error, world, names) {
+  if (error) throw error;
+
+  var globe = {type: "Sphere"},
+      land = topojson.feature(world, world.objects.land),
+      countries = topojson.feature(world, world.objects.countries).features,
+      borders = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }),
+      i = -1,
+      n = countries.length;
+
+  countries = countries.filter(function(d) {
+    return names.some(function(n) {
+      if (d.id == n.id) return d.name = n.name;
+    });
+  }).sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+
+  (function transition() {
+    d3.transition()
+        .duration(1250)
+        .each("start", function() {
+          title.text(countries[i = (i + 1) % n].name);
+        })
+        .tween("rotate", function() {
+          var p = d3.geo.centroid(countries[i]),
+              r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+          return function(t) {
+            projection.rotate(r(t));
+            c.clearRect(0, 0, width, height);
+            c.fillStyle = "#ccc", c.beginPath(), path(land), c.fill();
+            c.fillStyle = "#f00", c.beginPath(), path(countries[i]), c.fill();
+            c.strokeStyle = "#fff", c.lineWidth = .5, c.beginPath(), path(borders), c.stroke();
+            c.strokeStyle = "#000", c.lineWidth = 2, c.beginPath(), path(globe), c.stroke();
+          };
+        })
       .transition()
-        .attr("r", circle[2]);
-
-    // As we add more circles, generate more candidates per circle.
-    // Since this takes more effort, gradually reduce circles per frame.
-    if (k < 500) k *= 1.01, m *= .998;
-  }
-  return !n;
-});
-
-function bestCircleGenerator(maxRadius, padding) {
-  var quadtree = d3.geom.quadtree().extent([[0, 0], [width, height]])([]),
-      searchRadius = maxRadius * 2,
-      maxRadius2 = maxRadius * maxRadius;
-
-  return function(k) {
-    var bestX, bestY, bestDistance = 0;
-
-    for (var i = 0; i < k || bestDistance < padding; ++i) {
-      var x = Math.random() * width,
-          y = Math.random() * height,
-          rx1 = x - searchRadius,
-          rx2 = x + searchRadius,
-          ry1 = y - searchRadius,
-          ry2 = y + searchRadius,
-          minDistance = maxRadius; // minimum distance for this candidate
-
-      quadtree.visit(function(quad, x1, y1, x2, y2) {
-        if (p = quad.point) {
-          var p,
-              dx = x - p[0],
-              dy = y - p[1],
-              d2 = dx * dx + dy * dy,
-              r2 = p[2] * p[2];
-          if (d2 < r2) return minDistance = 0, true; // within a circle
-          var d = Math.sqrt(d2) - p[2];
-          if (d < minDistance) minDistance = d;
-        }
-        return !minDistance || x1 > rx2 || x2 < rx1 || y1 > ry2 || y2 < ry1; // or outside search radius
-      });
-
-      if (minDistance > bestDistance) bestX = x, bestY = y, bestDistance = minDistance;
-    }
-
-    var best = [bestX, bestY, bestDistance - padding];
-    quadtree.add(best);
-    return best;
-  };
+        .each("end", transition);
+  })();
 }
+
+d3.select(self.frameElement).style("height", height + "px");
